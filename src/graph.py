3c9104d1fd932684f618beb246fce25db4d53fa9
tanyaweaver <tanya83@uw.edu>
2016-08-29 16:10:09 -0700

    depth traversal tests are failing, have an infinite loop somewhere.

--- src/graph.py
+++ src/graph.py
@@ -11,21 +11,27 @@ class Graph(object):
         self._dict = {}
         try:
             for item in iterable:
-                self._dict.setdefault(str(item), [])
+                try:
+                    self._dict.setdefault((item), [])
+                except TypeError as e:
+                    raise TypeError(
+                        'Node must be an immutable data'
+                        ' type (string, integer, tuple, etc.'
+                    ).with_traceback(e.__traceback__)
         except TypeError:
             if iterable is not None:
-                self._dict.setdefault(str(iterable), [])
+                self._dict.setdefault((iterable), [])
 
     def add_node(self, n):
         """Add a node to graph"""
-        return self._dict.setdefault(str(n), [])
+        return self._dict.setdefault((n), [])
 
     def add_edge(self, n1, n2):
         """Add a edge from n1 to n2"""
-        new_node = self._dict.setdefault(str(n1), [])
-        self._dict.setdefault(str(n2), [])
-        if str(n2) not in new_node:
-            new_node.append(str(n2))
+        new_node = self._dict.setdefault((n1), [])
+        self._dict.setdefault((n2), [])
+        if (n2) not in new_node:
+            new_node.append((n2))
         else:
             raise ValueError('This edge already exists.')
 
@@ -44,45 +50,69 @@ class Graph(object):
 
     def del_node(self, n):
         """Delete a node from graph"""
-        if str(n) in self._dict:
-            del self._dict[str(n)]
+        if (n) in self._dict:
+            del self._dict[(n)]
             for key in self._dict:
-                if str(n) in self._dict[key]:
+                if (n) in self._dict[key]:
                     node_value = self._dict[key]
-                    node_value.remove(str(n))
+                    node_value.remove((n))
         else:
             raise KeyError('No such node in the graph.')
 
     def del_edge(self, n1, n2):
         """Delete a edge from n1 to n2"""
         try:
-            self._dict[str(n1)].remove(str(n2))
+            self._dict[(n1)].remove((n2))
         except KeyError:
             raise ValueError('No such edge exists')
 
     def has_node(self, n):
         """Check if n is a node of graph"""
-        if str(n) in self._dict.keys():
+        if (n) in self._dict.keys():
             return True
         else:
             return False
 
     def neighbors(self, n):
         """Return a list of nodes that have edge connect to n"""
-        try:
-            self._dict[str(n)]
-            neighbors = []
-            for pair in self._dict.items():
-                if str(n) in pair[1]:
-                    neighbors.append(pair[0])
-            return neighbors
-        except KeyError:
-            raise ValueError('Node not in the graph')
+        if n in self._dict:
+            return self._dict[(n)]
+        else:
+            raise KeyError('Node not in the graph')
 
     def adjacent(self, n1, n2):
         """Check if 2 node has connection"""
         try:
-            return str(n2) in self._dict[str(n1)] or\
-             str(n1) in self._dict[str(n2)]
+            return (n2) in self._dict[(n1)] or\
+             (n1) in self._dict[(n2)]
         except KeyError:
             raise ValueError('Node not in the graph')
+
+### Graph traversal
+
+    def depth_first_traversal(self, start):
+        """
+        Perform a full depth-traversal of the graph beggining at start.
+        Return full visited path when traversal is complete.
+        """
+        path_list = [start]
+        current_node = start
+        parent_list = []
+        flag1 = True
+        flag2 = True
+        while flag1:
+            neighbor = self.neighbors(current_node)
+            while flag2 and len(neighbor) != 0:
+                for n in neighbor:
+                    if n not in path_list:
+                        path_list.append(n)
+                        parent_list.append(current_node)
+                        current_node = n
+                        neighbor = self.neighbors(current_node)
+                        break
+                if len(parent_list) != 0:
+                    current_node = parent_list.pop()
+                    flag2 = False
+                else:
+                    flag1 = False
+        return path_list
--- src/test_graph.py
+++ src/test_graph.py
@@ -6,285 +6,303 @@ import pytest
 from graph import Graph
 
 
-INIT_VAL = [
-    (1),
-    (1, 123, 'a', '[1, 2, 3]'),
-    (1, 1, 1),
-    ((1, 2, 3), (2, 3, 4)),
-    (("{'a': 'b'}", 'test'))
+# INIT_VAL = [
+#     (1),
+#     (1, 123, 'a', '[1, 2, 3]'),
+#     (1, 1, 1),
+#     ((1, 2, 3), (2, 3, 4)),
+#     (("{'a': 'b'}", 'test'))
+# ]
+
+# LENGTH = [1, 4, 1, 2, 2]
+
+# INIT_LENGTH = zip(INIT_VAL, LENGTH)
+
+
+# ADD_NODE = [
+#    (10,),
+#    (10, 20, 30, 10),
+#    ('a', 'b', 'a'),
+#    ('[1, 2]', '[1, 2]'),
+#    ([1, 2, 3], {'a': 3}),
+# ]
+
+# ADD_LENGTH = [2, 7, 3, 3, 4]
+
+# ITER_ADD_NODE = zip(INIT_VAL, ADD_NODE)
+
+# ITER_ADD_LEN = zip(INIT_VAL, ADD_NODE, ADD_LENGTH)
+
+# DEL_LENGTH = zip(INIT_VAL, ADD_NODE, LENGTH)
+
+# ADD_EDGE = [
+#     (
+#         [1, 2, 3],
+#         ((1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)),
+#     ),
+# ]
+
+# ADD_EDGE_ALREADY_EXIST = [
+#     (
+#         [1, 2, 3],
+#         ((1, 2), (1, 3), (1, 2), (2, 1), (2, 3), (2, 3), (3, 1), (3, 2)),
+#     ),
+# ]
+
+# HAS_NODE_TABLE = [
+#     ([2, 3, 4], 4, True),
+#     ([2, 3, 4], 5, False),
+#     ('something', 2, False),
+#     (3, 4, False)
+# ]
+
+# NEIGHBORS_TABLE = [
+#     ([2, 3, 4], 4, 5),
+#     (None, 4, 5),
+#     ([2, 3], 4, 5),
+#     ('something', 4, 5)
+# ]
+
+# NEIGHBORS_ERROR_TABLE = [
+#     ([3, 4, 5], 6),
+#     (['something'], 6),
+#     ([100], 0),
+#     (None, 6),
+# ]
+
+# ADJACENT_TABLE = [
+#     ([3, 4, 5], 4, 5, False),
+#     (['something', 'else'], 'something', 'else', False)
+# ]
+
+# ADJACENT_TABLE1 = [
+#     ([2, 3, 4], 3, 4, True),
+#     (None, 3, 4, True),
+# ]
+
+# ADJACENT_ERROR_TABLE = [
+#     ([2, 3, 4], 5, 6),
+#     (None, 'anything', 'random'),
+#     ([], 'a', 'b'),
+# ]
+
+# DEL_EDGE_ERROR = [
+#     ([2, 3, 4], 5),
+#     ([], 'empty'),
+#     (None, 'something'),
+#     (['something', 'else', 4], 'random'),
+# ]
+
+
+# def test_init():
+#     gr = Graph()
+#     assert len(gr._dict.keys()) == 0
+
+
+# def test_init_with_none():
+#     gr = Graph(None)
+#     assert len(gr._dict.keys()) == 0
+
+
+# @pytest.mark.parametrize('iterable, length', INIT_LENGTH)
+# def test_init_iterable_length(iterable, length):
+#     gr = Graph(iterable)
+#     assert len(gr._dict.keys()) == length
+
+
+# @pytest.mark.parametrize('iterable', INIT_VAL)
+# def test_init_iterable_values(iterable):
+#     gr = Graph(iterable)
+#     for pair in gr._dict.items():
+#         assert pair[1] == []
+
+
+# @pytest.mark.parametrize('iterable', INIT_VAL)
+# def test_nodes(iterable):
+#     gr = Graph(iterable)
+#     try:
+#         for key in iterable:
+#             assert str(key) in gr._dict.keys()
+#     except TypeError:
+#         assert str(iterable) in gr._dict.keys()
+
+
+# def test_nodes_empty():
+#     gr = Graph()
+#     assert len(gr._dict.keys()) == 0
+
+
+# @pytest.mark.parametrize('iterable, edges', ADD_EDGE)
+# def test_add_edges(iterable, edges):
+#     gr = Graph(iterable)
+#     for edge in edges:
+#         gr.add_edge(edge[0], edge[1])
+#     for edge in edges:
+#             assert Node(edge[1]) in gr._dict[Node(edge[0])]
+
+
+# @pytest.mark.parametrize('iterable, edges', ADD_EDGE)
+# def test_add_edges_list_len(iterable, edges):
+#     gr = Graph(iterable)
+#     for edge in edges:
+#         gr.add_edge(edge[0], edge[1])
+#     for key in gr._dict:
+#         assert len(gr._dict[key]) == 2
+
+
+# @pytest.mark.parametrize('iterable, edges', ADD_EDGE_ALREADY_EXIST)
+# def test_add_edges_already_exist_list_len(iterable, edges):
+#     gr = Graph(iterable)
+#     try:
+#         for edge in edges:
+#             gr.add_edge(edge[0], edge[1])
+#     except ValueError:
+#         pass
+#     else:
+#         for key in gr._dict:
+#             assert len(gr._dict[key]) == 2
+
+# @pytest.mark.parametrize('iterable, edges', ADD_EDGE)
+# def test_edges(iterable, edges):
+#     gr = Graph(iterable)
+#     for edge in edges:
+#         gr.add_edge(edge[0], edge[1])
+#     for edge in edges:
+#         assert (Node(edge[0]), Node(edge[1])) in gr.edges()
+
+
+# @pytest.mark.parametrize('iterable, add_node', ITER_ADD_NODE)
+# def test_add_node_key(iterable, add_node):
+#     gr = Graph(iterable)
+#     for node in add_node:
+#         gr.add_node(node)
+#     for node in add_node:
+#         assert Node(node) in list(gr._dict.keys())
+
+
+# @pytest.mark.parametrize('iterable, add_node, length', ITER_ADD_LEN)
+# def test_add_node_length(iterable, add_node, length):
+#     gr = Graph(iterable)
+#     for node in add_node:
+#         gr.add_node(node)
+#     assert len(gr._dict.keys()) == length
+
+
+# @pytest.mark.parametrize('iterable, add_node', ITER_ADD_NODE)
+# def test_add_node_values(iterable, add_node):
+#     gr = Graph(iterable)
+#     for node in add_node:
+#         gr.add_node(node)
+#     for pair in gr._dict.items():
+#         assert pair[1] == []
+
+
+# def test_del_non_ex_node():
+#     gr = Graph([1, 2, 3, 4])
+#     with pytest.raises(KeyError):
+#         gr.del_node(10)
+
+
+# @pytest.mark.parametrize('iterable', INIT_VAL)
+# def test_del_node_key(iterable):
+#     gr = Graph(iterable)
+#     gr.add_node(100)
+#     gr.del_node(100)
+#     assert 100 not in gr._dict.keys()
+
+
+# @pytest.mark.parametrize('iterable, length', INIT_LENGTH)
+# def test_del_node_length(iterable, length):
+#     gr = Graph(iterable)
+#     gr.add_node(100)
+#     gr.del_node(100)
+#     assert len(gr._dict.keys()) == length
+
+
+# def test_del_node_edges():
+#     gr = Graph([1, 2, 3, 4])
+#     gr.add_node(100)
+#     gr.add_edge(1, 100)
+#     gr.add_edge(2, 100)
+#     gr.del_node(100)
+#     for edge in gr.edges():
+#         assert 100 not in edge
+
+
+# def test_del_edge():
+#     g = Graph()
+#     g.add_edge(3, 4)
+#     g.add_edge(3, 5)
+#     g.add_edge(3, 6)
+#     g.add_edge(7, 8)
+#     g.del_edge(7, 8)
+#     assert g.edges() == [
+#             (3, 4),
+#             (3, 5),
+#             (3, 6),
+#     ]
+
+
+# @pytest.mark.parametrize('iterable, n', DEL_EDGE_ERROR)
+# def test_del_edge_error(iterable, n):
+#     g = Graph(iterable)
+#     with pytest.raises(ValueError):
+#         g.del_edge(n, 'random')
+
+
+# @pytest.mark.parametrize('iterable, n, result', HAS_NODE_TABLE)
+# def test_has_node(iterable, n, result):
+#     g = Graph(iterable)
+#     assert g.has_node(n) == result
+
+
+# @pytest.mark.parametrize('iterable, n1, n2', NEIGHBORS_TABLE)
+# def test_neighbors(iterable, n1, n2):
+#     g = Graph(iterable)
+#     g.add_edge(n2, n1)
+#     assert Node(n2) in g.neighbors(n1)
+
+
+# @pytest.mark.parametrize('iterable, n', NEIGHBORS_ERROR_TABLE)
+# def test_neighbors_error(iterable, n):
+#     g = Graph(iterable)
+#     with pytest.raises(ValueError):
+#         g.neighbors(n)
+
+
+# @pytest.mark.parametrize('iterable, n1, n2, result', ADJACENT_TABLE)
+# def test_adjacent_false(iterable, n1, n2, result):
+#     g = Graph(iterable)
+#     assert g.adjacent(n1, n2) == result
+
+
+# @pytest.mark.parametrize('iterable, n1, n2, result', ADJACENT_TABLE1)
+# def test_adjacent(iterable, n1, n2, result):
+#     g = Graph(iterable)
+#     g.add_edge(n1, n2)
+#     assert g.adjacent(n1, n2) == result
+
+
+# @pytest.mark.parametrize('iterable, n1, n2', ADJACENT_ERROR_TABLE)
+# def test_adjacent_error(iterable, n1, n2):
+#     g = Graph(iterable)
+#     with pytest.raises(ValueError):
+#         g.adjacent(n1, n2)
+
+
+
+
+DEPTH_TR = [
+    ((1, 2, 3), 1, (1, 2, 3)),
+    ((1, 2, 3), 3, (3, 2)),
 ]
 
-LENGTH = [1, 4, 1, 2, 2]
 
-INIT_LENGTH = zip(INIT_VAL, LENGTH)
 
-
-ADD_NODE = [
-   (10,),
-   (10, 20, 30, 10),
-   ('a', 'b', 'a'),
-   ('[1, 2]', '[1, 2]'),
-   ([1, 2, 3], {'a': 3}),
-]
-
-ADD_LENGTH = [2, 7, 3, 3, 4]
-
-ITER_ADD_NODE = zip(INIT_VAL, ADD_NODE)
-
-ITER_ADD_LEN = zip(INIT_VAL, ADD_NODE, ADD_LENGTH)
-
-DEL_LENGTH = zip(INIT_VAL, ADD_NODE, LENGTH)
-
-ADD_EDGE = [
-    (
-        [1, 2, 3],
-        ((1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)),
-    ),
-]
-
-ADD_EDGE_ALREADY_EXIST = [
-    (
-        [1, 2, 3],
-        ((1, 2), (1, 3), (1, 2), (2, 1), (2, 3), (2, 3), (3, 1), (3, 2)),
-    ),
-]
-
-HAS_NODE_TABLE = [
-    ([2, 3, 4], 4, True),
-    ([2, 3, 4], 5, False),
-    ('something', 2, False),
-    (3, 4, False)
-]
-
-NEIGHBORS_TABLE = [
-    ([2, 3, 4], 4, 5),
-    (None, 4, 5),
-    ([2, 3], 4, 5),
-    ('something', 4, 5)
-]
-
-NEIGHBORS_ERROR_TABLE = [
-    ([3, 4, 5], 6),
-    (['something'], 6),
-    ([100], 0),
-    (None, 6),
-]
-
-ADJACENT_TABLE = [
-    ([3, 4, 5], 4, 5, False),
-    (['something', 'else'], 'something', 'else', False)
-]
-
-ADJACENT_TABLE1 = [
-    ([2, 3, 4], 3, 4, True),
-    (None, 3, 4, True),
-]
-
-ADJACENT_ERROR_TABLE = [
-    ([2, 3, 4], 5, 6),
-    (None, 'anything', 'random'),
-    ([], 'a', 'b'),
-]
-
-DEL_EDGE_ERROR = [
-    ([2, 3, 4], 5),
-    ([], 'empty'),
-    (None, 'something'),
-    (['something', 'else', 4], 'random'),
-]
-
-
-def test_init():
-    gr = Graph()
-    assert len(gr._dict.keys()) == 0
-
-
-def test_init_with_none():
-    gr = Graph(None)
-    assert len(gr._dict.keys()) == 0
-
-
-@pytest.mark.parametrize('iterable, length', INIT_LENGTH)
-def test_init_iterable_length(iterable, length):
-    gr = Graph(iterable)
-    assert len(gr._dict.keys()) == length
-
-
-@pytest.mark.parametrize('iterable', INIT_VAL)
-def test_init_iterable_values(iterable):
-    gr = Graph(iterable)
-    for pair in gr._dict.items():
-        assert pair[1] == []
-
-
-@pytest.mark.parametrize('iterable', INIT_VAL)
-def test_nodes(iterable):
-    gr = Graph(iterable)
-    try:
-        for key in iterable:
-            assert str(key) in gr._dict.keys()
-    except TypeError:
-        assert str(iterable) in gr._dict.keys()
-
-
-def test_nodes_empty():
-    gr = Graph()
-    assert len(gr._dict.keys()) == 0
-
-
-@pytest.mark.parametrize('iterable, edges', ADD_EDGE)
-def test_add_edges(iterable, edges):
-    gr = Graph(iterable)
-    for edge in edges:
-        gr.add_edge(edge[0], edge[1])
-    for edge in edges:
-            assert str(edge[1]) in gr._dict[str(edge[0])]
-
-
-@pytest.mark.parametrize('iterable, edges', ADD_EDGE)
-def test_add_edges_list_len(iterable, edges):
-    gr = Graph(iterable)
-    for edge in edges:
-        gr.add_edge(edge[0], edge[1])
-    for key in gr._dict:
-        assert len(gr._dict[key]) == 2
-
-
-@pytest.mark.parametrize('iterable, edges', ADD_EDGE_ALREADY_EXIST)
-def test_add_edges_already_exist_list_len(iterable, edges):
-    gr = Graph(iterable)
-    try:
-        for edge in edges:
-            gr.add_edge(edge[0], edge[1])
-    except ValueError:
-        pass
-    else:
-        for key in gr._dict:
-            assert len(gr._dict[key]) == 2
-
-@pytest.mark.parametrize('iterable, edges', ADD_EDGE)
-def test_edges(iterable, edges):
-    gr = Graph(iterable)
-    for edge in edges:
-        gr.add_edge(edge[0], edge[1])
-    for edge in edges:
-        assert (str(edge[0]), str(edge[1])) in gr.edges()
-
-
-@pytest.mark.parametrize('iterable, add_node', ITER_ADD_NODE)
-def test_add_node_key(iterable, add_node):
-    gr = Graph(iterable)
-    for node in add_node:
-        gr.add_node(node)
-    for node in add_node:
-        assert str(node) in list(gr._dict.keys())
-
-
-@pytest.mark.parametrize('iterable, add_node, length', ITER_ADD_LEN)
-def test_add_node_length(iterable, add_node, length):
-    gr = Graph(iterable)
-    for node in add_node:
-        gr.add_node(node)
-    assert len(gr._dict.keys()) == length
-
-
-@pytest.mark.parametrize('iterable, add_node', ITER_ADD_NODE)
-def test_add_node_values(iterable, add_node):
-    gr = Graph(iterable)
-    for node in add_node:
-        gr.add_node(node)
-    for pair in gr._dict.items():
-        assert pair[1] == []
-
-
-def test_del_non_ex_node():
-    gr = Graph([1, 2, 3, 4])
-    with pytest.raises(KeyError):
-        gr.del_node(10)
-
-
-@pytest.mark.parametrize('iterable', INIT_VAL)
-def test_del_node_key(iterable):
-    gr = Graph(iterable)
-    gr.add_node(100)
-    gr.del_node(100)
-    assert '100' not in gr._dict.keys()
-
-
-@pytest.mark.parametrize('iterable, length', INIT_LENGTH)
-def test_del_node_length(iterable, length):
-    gr = Graph(iterable)
-    gr.add_node(100)
-    gr.del_node(100)
-    assert len(gr._dict.keys()) == length
-
-
-def test_del_node_edges():
-    gr = Graph([1, 2, 3, 4])
-    gr.add_node(100)
-    gr.add_edge(1, 100)
-    gr.add_edge(2, 100)
-    gr.del_node(100)
-    for edge in gr.edges():
-        assert '100' not in edge
-
-
-def test_del_edge():
-    g = Graph()
-    g.add_edge(3, 4)
-    g.add_edge(3, 5)
-    g.add_edge(3, 6)
-    g.add_edge(7, 8)
-    g.del_edge(7, 8)
-    assert g.edges() == [
-            ('3', '4'),
-            ('3', '5'),
-            ('3', '6'),
-    ]
-
-
-@pytest.mark.parametrize('iterable, n', DEL_EDGE_ERROR)
-def test_del_edge_error(iterable, n):
-    g = Graph(iterable)
-    with pytest.raises(ValueError):
-        g.del_edge(n, 'random')
-
-
-@pytest.mark.parametrize('iterable, n, result', HAS_NODE_TABLE)
-def test_has_node(iterable, n, result):
-    g = Graph(iterable)
-    assert g.has_node(n) == result
-
-
-@pytest.mark.parametrize('iterable, n1, n2', NEIGHBORS_TABLE)
-def test_neighbors(iterable, n1, n2):
-    g = Graph(iterable)
-    g.add_edge(n2, n1)
-    assert str(n2) in g.neighbors(n1)
-
-
-@pytest.mark.parametrize('iterable, n', NEIGHBORS_ERROR_TABLE)
-def test_neighbors_error(iterable, n):
-    g = Graph(iterable)
-    with pytest.raises(ValueError):
-        g.neighbors(n)
-
-
-@pytest.mark.parametrize('iterable, n1, n2, result', ADJACENT_TABLE)
-def test_adjacent_false(iterable, n1, n2, result):
-    g = Graph(iterable)
-    assert g.adjacent(n1, n2) == result
-
-
-@pytest.mark.parametrize('iterable, n1, n2, result', ADJACENT_TABLE1)
-def test_adjacent(iterable, n1, n2, result):
-    g = Graph(iterable)
-    g.add_edge(n1, n2)
-    assert g.adjacent(n1, n2) == result
-
-
-@pytest.mark.parametrize('iterable, n1, n2', ADJACENT_ERROR_TABLE)
-def test_adjacent_error(iterable, n1, n2):
-    g = Graph(iterable)
-    with pytest.raises(ValueError):
-        g.adjacent(n1, n2)
+@pytest.mark.parametrize('iterable, start, path_list', DEPTH_TR)
+def test_depth_first_traversal(iterable, start, path_list):
+    gr = Graph([iterable])
+    gr.add_edge(1, 2)
+    gr.add_edge(1, 3)
+    gr.add_edge(3, 2)
+    assert gr.depth_first_traversal(start) == path_list
